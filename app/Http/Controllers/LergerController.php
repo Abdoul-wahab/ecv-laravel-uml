3a2e387b8b5d5958afb5c9db39c74115dceb7cd9
abdoulzkr <abdwahab.zkr@gmail.com>
2021-05-27 23:10:45 +0200

    create models, migrations and controllers with crud

---
+++ app/Http/Controllers/LergerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lerger;
+use Illuminate\Http\Request;
+
+class LergerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lerger  $lerger
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lerger $lerger)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lerger  $lerger
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lerger $lerger)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lerger  $lerger
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lerger $lerger)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lerger  $lerger
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lerger $lerger)
+    {
+        //
+    }
+}
