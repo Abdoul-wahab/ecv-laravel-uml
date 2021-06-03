add7745957346b1a79f800dca5809187ee36757e
Chris <christopher.dolbert@vinci-immobilier.com>
2021-06-03 14:41:53 +0200

    Upd WEB & SIDEBAR

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -47,7 +47,7 @@
                     <div class="flex-grow mt-5 flex flex-col">
                         <nav class="flex-1 bg-white px-2 space-y-1">
                             <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
-                            <a href="{{ route('index') }}" class="@if (Route::current()->getName() === 'dashboard') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
+                            <a href="{{ route('vehicule.index') }}" class="@if (Route::current()->getName() === 'dashboard') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
                                 <!-- Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500" -->
                                 <svg class="@if (Route::current()->getName() === 'controle') text-gray-500 @else text-gray-400 group-hover:text-gray-500 @endif mr-3 h-6 w-6" x-description="Heroicon name: outline/home"
                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -61,7 +61,7 @@
 
 
                             <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
-                            <a href="#" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
+                            <a href="{{ route('vehicule-create') }}" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
                                 <!-- Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500" -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="@if (Route::current()->getName() === 'controle') text-gray-500 @else text-gray-400 group-hover:text-gray-500 @endif mr-3 h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
                                     <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z" />
@@ -69,16 +69,16 @@
                                 Ajouter un vehicule
                             </a>
 
-                            <a href="#" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
+                            <a href="{{ route('controles.index') }}" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
 
                                 <svg xmlns="http://www.w3.org/2000/svg" class="@if (Route::current()->getName() === 'controle') text-gray-500 @else text-gray-400 group-hover:text-gray-500 @endif mr-3 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                 </svg>
-                                Contrôler
+                                Contrôles
                             </a>
 
-                            <a href="#" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
+                            <a href="{{ route('employes.index') }}" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
                                 <svg class="@if (Route::current()->getName() === 'controle') text-gray-500 @else text-gray-400 group-hover:text-gray-500 @endif mr-3 h-6 w-6" x-description="Heroicon name: outline/folder" xmlns="http://www.w3.org/2000/svg" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -88,7 +88,7 @@
                                 Employés
                             </a>
 
-                            <a href="#" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
+                            <a href="{{ route('contrats.index') }}" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
                                 <svg class="@if (Route::current()->getName() === 'controle') text-gray-500 @else text-gray-400 group-hover:text-gray-500 @endif mr-3 h-6 w-6" x-description="Heroicon name: outline/calendar" xmlns="http://www.w3.org/2000/svg" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -98,14 +98,14 @@
                                 Contrats
                             </a>
 
-                            <a href="#" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
+                        <a href="{{ route('locations.index') }}" class="@if (Route::current()->getName() === 'ajouter') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium ">
                                 <svg class="@if (Route::current()->getName() === 'controle') text-gray-500 @else text-gray-400 group-hover:text-gray-500 @endif mr-3 h-6 w-6" x-description="Heroicon name: outline/inbox" xmlns="http://www.w3.org/2000/svg" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4">
                                     </path>
                                 </svg>
-                                Contrats
+                                Locations
                             </a>
 
                             <a href="{{ route('logout') }}" class="@if (Route::current()->getName() === 'logout') bg-gray-100 text-gray-900 @else text-gray-600 hover:bg-gray-50 hover:text-gray-900 @endif group rounded-md py-2 px-2 flex items-center text-sm font-medium "
--- routes/web.php
+++ routes/web.php
@@ -55,34 +55,34 @@ Route::get('/contrat-details/{contrat}', [ContratController::class, 'show'])->na
 Route::get('/contrat-create', [ContratController::class, 'create'])->name('contrat-create');
 Route::get('/edit-contrat/{contrat}', [ContratController::class, 'edit'])->name('edit-contrat');
 Route::get('/update-contrat', [ContratController::class, 'update'])->name('update-contrat');
-Route::get('/contrats', [ContratController::class, 'index'])->name('index');
+Route::get('/contrats', [ContratController::class, 'index'])->name('contrats.index');
 Route::post('/contrat-store', [ContratController::class, 'store'])->name('contrat-store');
 
 Route::get('/controle-details/{controle}', [ControleController::class, 'show'])->name('controle-details');
 Route::get('/controle-create', [ControleController::class, 'create'])->name('controle-create');
 Route::get('/edit-controle/{controle}', [ControleController::class, 'edit'])->name('edit-controle');
 Route::get('/update-controle', [ControleController::class, 'update'])->name('update-controle');
-Route::get('/controles', [ControleController::class, 'index'])->name('index');
+Route::get('/controles', [ControleController::class, 'index'])->name('controles.index');
 Route::post('/controle-store', [ControleController::class, 'store'])->name('controle-store');
 
 Route::get('/employe-details/{employe}', [EmployeController::class, 'show'])->name('employe-details');
 Route::get('/employe-create', [EmployeController::class, 'create'])->name('employe-create');
 Route::get('/edit-employe/{employe}', [EmployeController::class, 'edit'])->name('edit-employe');
 Route::get('/update-employe', [EmployeController::class, 'update'])->name('update-employe');
-Route::get('/employes', [EmployeController::class, 'index'])->name('index');
+Route::get('/employes', [EmployeController::class, 'index'])->name('employes.index');
 Route::post('/employe-store', [EmployeController::class, 'store'])->name('employe-store');
 
 Route::get('/location-details/{employe}', [LocationController::class, 'show'])->name('location-details');
 Route::get('/location-create', [LocationController::class, 'create'])->name('location-create');
 Route::get('/edit-location/{employe}', [LocationController::class, 'edit'])->name('edit-location');
 Route::get('/update-location', [LocationController::class, 'update'])->name('update-location');
-Route::get('/locations', [LocationController::class, 'index'])->name('index');
+Route::get('/locations', [LocationController::class, 'index'])->name('locations.index');
 Route::post('/location-store', [LocationController::class, 'store'])->name('location-store');
 
 Route::get('/vehicule-details/{vehicule}', [VehiculeController::class, 'show'])->name('vehicule-details');
 Route::get('/vehicule-create', [VehiculeController::class, 'create'])->name('vehicule-create');
 Route::get('/edit-vehicule/{vehicule}', [VehiculeController::class, 'edit'])->name('edit-vehicule');
 Route::get('/update-vehicule', [VehiculeController::class, 'update'])->name('update-vehicule');
-Route::get('/vehicules', [VehiculeController::class, 'index'])->name('index');
+Route::get('/vehicules', [VehiculeController::class, 'index'])->name('vehicules.index');
 Route::post('/vehicule-store', [VehiculeController::class, 'store'])->name('vehicule-store');
 
