4c169d8b41dc0ae61767cc6404686fba694ea5d7
Chris <christopher.dolbert@vinci-immobilier.com>
2021-06-03 16:07:27 +0200

    Up

--- resources/views/contrat/contrat-details.blade.php
+++ resources/views/contrat/contrat-details.blade.php
@@ -10,7 +10,9 @@
             </div>
             <div class="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
                 <h3 class="text-gray-700 uppercase text-lg">Ma voiture test</h3>
-                <span class="text-gray-500 mt-3">$125</span>
+                <span class="text-gray-500 mt-3">{{contrat->prix}}</span>
+                <span class="text-gray-500 mt-3">{{contrat->date}}</span>
+                <span class="text-gray-500 mt-3">{{contrat->content}}</span>
                 <hr class="my-3">
                 {{-- <div class="mt-2">
                     <label class="text-gray-700 text-sm" for="count">Nombre de véhicules souhaité:</label>
